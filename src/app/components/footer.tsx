b85093d8e99c69781bdd5fc9ea77fe8e97b37451
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-05-22 20:35:41 +0300

    changed icon sizing

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center mt-20 bg-purple w-full p-10 font-accent text-beige text-3xl font-outline-1 text-center">
       It was great having you here!
-      <div className="flex flex-row items-center justify-between gap-x-12 mt-5 text-black">
+      <div className="flex flex-row items-center justify-between gap-x-12 mt-10 text-black text-4xl">
         <MdOutlineAlternateEmail />
         <TfiLinkedin />
         <FaGithub />
